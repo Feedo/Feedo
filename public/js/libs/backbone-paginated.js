bd17b5c87b42673df8e6d6ed79dfe399243d0e20
Jan-Henrik Bruhn <jh.bruhn@me.com>
2013-06-26 21:34:19 +0200

    removing pageInfo from backbone-paginated

--- public/js/libs/backbone-paginated.js
+++ public/js/libs/backbone-paginated.js
@@ -1,6 +1,6 @@
 var PaginatedCollection = Backbone.Collection.extend({
   initialize: function() {
-    _.bindAll(this, 'parse', 'pageInfo', 'nextPage', 'previousPage');
+    _.bindAll(this, 'parse', 'nextPage', 'previousPage');
     typeof(options) != 'undefined' || (options = {});
     this.page = 1;
     typeof(this.perPage) != 'undefined' || (this.perPage = 10);
@@ -20,51 +20,13 @@ var PaginatedCollection = Backbone.Collection.extend({
     return Backbone.Collection.prototype.fetch.call(this, options);
   },
   parse: function(resp) {
-    /*this.page = resp.page;
-    this.perPage = resp.perPage;
-    this.total = resp.total;*/
-    console.log(resp);
     return resp;
   },
-  pageInfo: function() {
-    var info = {
-      total: this.total,
-      page: this.page,
-      perPage: this.perPage,
-      pages: Math.ceil(this.total / this.perPage),
-      prev: false,
-      next: false
-    };
- 
-    var max = Math.min(this.total, this.page * this.perPage);
- 
-    if (this.total == this.pages * this.perPage) {
-      max = this.total;
-    }
- 
-    info.range = [(this.page - 1) * this.perPage + 1, max];
- 
-    if (this.page > 1) {
-      info.prev = this.page - 1;
-    }
- 
-    if (this.page < info.pages) {
-      info.next = this.page + 1;
-    }
- 
-    return info;
-  },
   nextPage: function() {
-    if (!this.pageInfo().next) {
-      return false;
-    }
     this.page = this.page + 1;
     return this.fetch();
   },
   previousPage: function() {
-    if (!this.pageInfo().prev) {
-      return false;
-    }
     this.page = this.page - 1;
     return this.fetch();
   }
