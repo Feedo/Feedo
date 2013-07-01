35051b51eee1f7bacacec1000956aef1077dd152
Jan-Henrik Bruhn <jh.bruhn@me.com>
2013-07-01 09:24:42 +0200

    implemented favicon fetching

--- models/feed.rb
+++ models/feed.rb
@@ -1,5 +1,7 @@
+require 'pismo'
+
 class Feed < ActiveRecord::Base
-  attr_accessible :file_url, :title, :link, :description
+  attr_accessible :file_url, :title, :link, :description, :favicon_url
   has_many :feed_items
   
   validates :file_url, :presence => true
@@ -10,18 +12,25 @@ class Feed < ActiveRecord::Base
     begin     
       Feedo.logger.info "Checking "+self.file_url+"..."
       feedzirra = Feedzirra::Feed.fetch_and_parse(self.file_url)
+      
       self.title = feedzirra.title
       self.link = feedzirra.url
       self.description = feedzirra.description
-    
+      
+      # fetch the favicon
+      doc = Pismo::Document.new(self.link)
+      
+      self.favicon_url = doc.favicon unless doc.favicon.nil?
+      self.favicon_url = self.link + "/favicon.ico" if doc.favicon.nil?
+      
       feedzirra.entries.each do |entry|
         FeedItem.insert_or_update(self, entry)
       end
       save!
       Feedo.logger.info "Checked "+self.file_url+"."
     
-    rescue
-      Feedo.logger.error "#{self.file_url} is invalid!"
+    rescue Exception => e
+      Feedo.logger.error "#{self.file_url} is invalid! #{e}"
     end
   end
   
