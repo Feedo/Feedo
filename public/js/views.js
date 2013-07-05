e9a389a0678ed3544cbf03eeebc51f6c3256cdf6
Sören Gade <gade.soeren@googlemail.com>
2013-07-05 13:07:34 +0200

    Added add feed status code handling.

--- public/js/views.js
+++ public/js/views.js
@@ -341,7 +341,20 @@ var AppView = Backbone.View.extend({
         },
         // Server-side guy messed up again
         error: function(model, xhr, options) {
-          alert("Something went wrong adding the feed.");
+          var status = xhr.status;
+          
+          switch ( status ) {
+            case 400: // Bad request
+              alert("This seems to be an invalid feed.");
+              break;
+            case 409: // Conflict
+              alert("This feed is already in the database.");
+              break;
+              
+            default:
+              alert("Someting went wrong adding the feed: " + status + ' ("' + xhr.statusText + '")');
+              break;
+          } 
           // enable controls again
           $("#modal-add-feed input, #modal-add-feed .btn").removeAttr('disabled');
         }
